32d06270905c6acc8e8b975bcd74c07b317b51a5
Nguyen Quoc Huy <ngquochuy4002@gmail.com>
2023-11-04 22:43:51 +0700

    chinh sua giao dien home lan 2

--- DO_An_Web/js/home.js
+++ DO_An_Web/js/home.js
@@ -19,3 +19,19 @@ document
   .addEventListener("click", function () {
     window.scrollTo({ top: 0, behavior: "smooth" });
   });
+
+const bar = document.getElementById("bar");
+const close = document.getElementById("close");
+const nav = document.getElementById("navbar");
+
+if (bar) {
+  bar.addEventListener("click", () => {
+    nav.classList.add("active");
+  });
+
+  if (close) {
+    close.addEventListener("click", () => {
+      nav.classList.remove("active");
+    });
+  }
+}
