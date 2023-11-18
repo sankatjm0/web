0ff03f280c38d3f9851930b7fd7d103f0cefc238
camdao <luongcamdao@gmail.com>
2023-11-18 21:05:20 +0700

    tim kiem click ra san pham

--- DO_An_Web/js/search.js
+++ DO_An_Web/js/search.js
@@ -1,3 +1,4 @@
+let products;
 let shop_listIteam = document.querySelector('.product-container');
 let filters = document.querySelectorAll('.shop-filter-group-list');
 let primary = document.getElementById('primary');
@@ -9,106 +10,32 @@ const search = document.getElementById('search-box');
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const searchValue = urlParams.get('query');
-        
-let listIteam = [
-    {
-        "id": "1",
-        "name": "a b line what the gucci",
-        "discounttag":50,
-        "Price": "20",
-        "actualprice":"40",
-        "category":"Man",
-        "traDemark":"color",
-        "image":"img/card2.png"
-    },
-    {
-        "id": "1",
-        "name": "a short line about the adidas",
-        "discounttag":50,
-        "Price": "30",
-        "actualprice":"60",
-        "category":"Woman",
-        "traDemark":"brand",
-        "image":"img/card3.png"
-    },
-    {
-        "id": "1",
-        "name": "a short line about the cloth",
-        "discounttag":50,
-        "Price": "20",
-        "actualprice":"40",
-        "category":"Woman",
-        "traDemark":"brand",
-        "image":"img/card4.png"
-    },
-    {
-        "id": "1",
-        "name": "a short line about the cloth",
-        "discounttag":50,
-        "Price": "20",
-        "actualprice":"40",
-        "category":"Woman",
-        "traDemark":"brand",
-        "image":"img/card4.png"
-    },
-    {
-        "id": "1",
-        "name": "a short line about the cloth",
-        "discounttag":50,
-        "Price": "20",
-        "actualprice":"40",
-        "category":"Woman",
-        "traDemark":"brand",
-        "image":"img/card5.png"
-    },
-    {
-        "id": "1",
-        "name": "a short line about the cloth",
-        "discounttag":50,
-        "Price": "20",
-        "actualprice":"40",
-        "category":"Woman",
-        "traDemark":"brand",
-        "image":"img/card6.png"
-    },
-    {
-        "id": "1",
-        "name": "a short line about the cloth",
-        "discounttag":50,
-        "Price": "20",
-        "actualprice":"40",
-        "category":"Woman",
-        "traDemark":"brand",
-        "image":"img/card7.png"
-    },
-    {
-        "id": "1",
-        "name": "a short line about the cloth",
-        "discounttag":50,
-        "Price": "20",
-        "actualprice":"40",
-        "category":"Woman",
-        "traDemark":"brand",
-        "image":"img/card8.png"
-    },
-    {
-        "id": "1",
-        "name": "a short line about the cloth",
-        "discounttag":50,
-        "Price": "20",
-        "actualprice":"40",
-        "category":"Woman",
-        "traDemark":"brand",
-        "image":"img/card9.png"
-    }
-];
+      
+window.onload = function () {
+    products = JSON.parse(localStorage.getItem("product")) || [];
+    Findname(" ");
+}
+// let listIteam = [
+//     {
+//         "name": "a b line what the gucci",
+//         "Price": "20",
+//         " ":"color",
+//         "image":"img/card2.png"
+//     }
+// ];
 let foundItems = [];
+
+document.querySelector('.product-container').addEventListener('click', function () {
+    const searchId = document.querySelector('.product-container').value;
+    window.location.href = 'product.html?query=' + encodeURIComponent(searchValue);
+});
 
 function Findname(Name){
     search.value=searchValue;
 
     foundItems = [];
-    listIteam.forEach(item => {
+    if(products !== undefined)
+    products.forEach(item => {
         if(item.name.includes(Name)){
             foundItems.push(item);
         }
@@ -149,14 +76,9 @@ function showProduct(list){
                 NewItem_product_image.className = "product-image";
                 NewItem.append(NewItem_product_image);
 
-                    let Newitem__span = document.createElement('span');
-                    Newitem__span.className = "discount-tag";
-                    Newitem__span.innerHTML=item.discounttag + '%';
-                    NewItem_product_image.append(Newitem__span);
-
                     let Newitem__img = document.createElement('img');
                     Newitem__img.className = "product-thumb";
-                    Newitem__img.src = item.image;
+                    Newitem__img.src = item.img;
                     Newitem__img.alt=""
                     NewItem_product_image.prepend(Newitem__img);
 
@@ -165,13 +87,13 @@ function showProduct(list){
                     New_button.innerHTML="add to whislist";
                     NewItem_product_image.append(New_button);
 
-                let product_info = document.createElement('div');
-                product_info.className = "product-info";
-                NewItem.append(product_info);
+                    let product_info = document.createElement('div');
+                    product_info.className = "product-info";
+                    NewItem.append(product_info);
 
                     let product_h2 = document.createElement('h2');
                     product_h2.className = "product-brand";
-                    product_h2.innerHTML=item.traDemark;
+                    product_h2.innerHTML=item.brand;
                     product_info.append(product_h2);
 
                     let product_p = document.createElement('p');
@@ -181,14 +103,8 @@ function showProduct(list){
 
                     let product_span_1 = document.createElement('span');
                     product_span_1.className = "price";
-                    product_span_1.innerHTML='$'+item.Price;
+                    product_span_1.innerHTML=item.price+'đ';
                     product_info.append(product_span_1);
-
-                    let product_span_2 = document.createElement('span');
-                    product_span_2.className = "actual-price";
-                    product_span_2.innerHTML='$'+item.actualprice;
-                    product_info.append(product_span_2);
-
         });
     }
 }
@@ -214,20 +130,13 @@ function show_filter(list) {
     });
 
     if (list.length != 0) {
-        let category = "";
         let traDemark = "";
-
-        let shop_filter_category = document.querySelector('.shop-filter-group-list.category');
         let shop_filter_traDemark = document.querySelector('.shop-filter-group-list.traDemark');
 
         list.forEach(item => {
-            if (!category.includes(item.category)) {
-                category = category + item.category;
-                createFilterItem(item.category, "category",shop_filter_category);
-            }
-            if (!traDemark.includes(item.traDemark)) {
-                traDemark = traDemark + item.traDemark;
-                createFilterItem(item.traDemark, "traDemark",shop_filter_traDemark);
+            if (!traDemark.includes(item.brand)) {
+                traDemark = traDemark + item.brand;
+                createFilterItem(item.brand, "traDemark",shop_filter_traDemark);
             }
         });
     }
@@ -250,14 +159,10 @@ function applyFilters(){
 
     let item = [];
     let item_Price = [];
-    let category = [];
     let traDemark = [];
     let ThapDenCao = false;
     let CaoDenThap = false;
     selectedCategories.forEach(check=>{
-        if(check.includes('category')){
-            category.push(check);
-        }
         if(check.includes('traDemark')){
             traDemark.push(check);
         }
@@ -269,33 +174,23 @@ function applyFilters(){
         }
     });
     
-    if(category.length==0 &&traDemark.length==0){
-        
+    if(traDemark.length==0){
+
     }
     else{
         foundItems.forEach(check=>{
             let checked =0;
-            if(category.length==0){
-                checked++;
-            }
-            for (let i = 0; i < category.length; i++) {
-                let category_item=category[i].replace(/category/, "");
-                if(check.category==category_item){
-                    checked++;
-                    break;
-                }
-            }
             if(traDemark.length==0){
                 checked++;
             }
             for (let i = 0; i < traDemark.length; i++) {
                 let traDemark_item=traDemark[i].replace(/traDemark/, "");
-                if(check.traDemark==traDemark_item){
+                if(check.brand==traDemark_item){
                     checked++;
                     break;
                 }
             }
-            if(checked==2){
+            if(checked==1){
                 item.push(check);
             }
             
@@ -309,20 +204,21 @@ function applyFilters(){
     if(number1.value === "" && number2.value === ""){
         if(item.length!=0){
             if(ThapDenCao){
-                item.sort((a, b) => parseFloat(a.Price) - parseFloat(b.Price));
+                item.sort((a, b) => parseFloat(a.price) - parseFloat(b.price));
             }
             if(CaoDenThap){
-                item.sort((b, a) => parseFloat(a.Price) - parseFloat(b.Price));
+                item.sort((b, a) => parseFloat(a.price) - parseFloat(b.price));
             }
             showProduct(item);
         }
         else{
             let item_i = [...foundItems];
             if(ThapDenCao){
-                item_i.sort((a, b) => parseFloat(a.Price) - parseFloat(b.Price));
+                item_i.sort((a, b) => parseFloat(a.price) - parseFloat(b.price));
+                console.log(item_i);
             }
             if(CaoDenThap){
-                item_i.sort((b, a) => parseFloat(a.Price) - parseFloat(b.Price));
+                item_i.sort((b, a) => parseFloat(a.price) - parseFloat(b.price));
             }
             showProduct(item_i);
         }
@@ -330,7 +226,7 @@ function applyFilters(){
     else{
         if(item.length!=0){
             item.forEach(checka=>{
-                if(parseFloat(checka.Price)>=parseFloat(number1.value) && parseFloat(checka.Price)<=parseFloat(number2.value)){
+                if(parseFloat(checka.price)>=parseFloat(number1.value) && parseFloat(checka.price)<=parseFloat(number2.value)){
                     item_Price.push(checka);
                 }
             });
@@ -338,16 +234,16 @@ function applyFilters(){
         else{
             let item_i = [...foundItems];
             item_i.forEach(checka=>{
-                if(parseFloat(checka.Price)>=parseFloat(number1.value) && parseFloat(checka.Price)<=parseFloat(number2.value)){
+                if(parseFloat(checka.price)>=parseFloat(number1.value) && parseFloat(checka.price)<=parseFloat(number2.value)){
                     item_Price.push(checka);
                 }
             });
         }
         if(ThapDenCao){
-            item_Price.sort((a, b) => parseFloat(a.Price) - parseFloat(b.Price));
+            item_Price.sort((a, b) => parseFloat(a.price) - parseFloat(b.price));
         }
         if(CaoDenThap){
-            item_Price.sort((b, a) => parseFloat(a.Price) - parseFloat(b.Price));
+            item_Price.sort((b, a) => parseFloat(a.price) - parseFloat(b.price));
         }
         if(item_Price.length!=0){
             showProduct(item_Price);
