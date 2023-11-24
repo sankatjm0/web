c2284572eca519dcb51a1f9b55cd4863cf070d6e
Hung dap chai <0258hahung@gmail.com>
2023-11-24 22:42:03 +0700

    Khanh

--- DO_An_Web/js/product.js
+++ DO_An_Web/js/product.js
@@ -3,9 +3,9 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const searchId = urlParams.get('ID');
 
-
 window.onload = function () {
   products = JSON.parse(localStorage.getItem("product")) || [];
+  productInCart = JSON.parse(localStorage.getItem("productInCart")) || [];
 }
 // size do
 window.addEventListener('load', function() {
@@ -67,9 +67,44 @@ function addProduct(item){
 
     <div class="btn-group">
         <br>
-        <buttom class="btn">Thêm vào vỏ hàng</buttom>
+        <buttom  onclick='addToCart(${item.ID})' class="btn">Thêm vào vỏ hàng</buttom>
     </div>
   </div>
   
   `;
 }
+// Khai báo mảng productInCart để lưu trữ sản phẩm trong giỏ hàng
+var productInCart = [];
+
+// Hàm thêm sản phẩm vào giỏ hàng
+function addToCart(ID) {
+  let checkProduct = productInCart.some(value => value.ID === ID);
+
+  if (!checkProduct) {
+      let product = products.find(value => value.ID === ID);
+      productInCart.unshift({
+          ...product,
+          quantity: 1
+      });
+  } else {
+      let product = productInCart.find(value => value.ID === ID);
+      let getIndex = productInCart.findIndex(value => value.ID === ID);
+      productInCart[getIndex] = {
+          ...product,
+          quantity: ++product.quantity
+      };
+  }
+
+  // Lưu mảng productInCart vào localStorage sau khi thêm sản phẩm
+  localStorage.setItem('productInCart', JSON.stringify(productInCart));
+}
+
+
+
+
+
+
+    
+
+
+
