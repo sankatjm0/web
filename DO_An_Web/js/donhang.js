68feffc93ef0c8a28e602a377d2dba9e551b6d57
thankhanh <hathanhkhanh55204@gmail.com>
2023-11-28 14:22:24 +0700

    xu li phan chuyen trang thai don hang

--- DO_An_Web/js/donhang.js
+++ DO_An_Web/js/donhang.js
@@ -3,31 +3,32 @@ let User = JSON.parse(localStorage.getItem("user")) || [];
 let donhang = document.getElementById("donhang");
 let content = document.getElementById("content");
 
-donhang.addEventListener('click', function() {
+donhang.addEventListener('click', function () {
     content.innerHTML = '';
     content.innerHTML = `
     <div class="donhang">
-    <div class="locsp">
-        <div class="thanhnav">
-            <div class="boloc">bộ lọc</div>
+        <div class="locsp">
+            <div class="thanhnav">
+                <div class="boloc">bộ lọc</div>
+            </div>
+            <div class="timsanpham">
+                <input type="text" placeholder="ten san pham">
+                <input type="text" placeholder="ten khac hang">
+                <div>thoigian</div>
+            </div>
         </div>
-        <div class="timsanpham">
-            <input type="text" placeholder="ten san pham">
-            <input type="text" placeholder="ten khac hang">
-            <div>thoigian</div>
-        </div>
-    </div>
 
-    <div class="thongtin">
-        <div class="thongtinsanpham">
-            <div class="hinh">IMG</div>
-            <div class="sanpham">sản phẩm</div>
-            <div class="dongia">đơn giá</div>
-            <div class="soluong">số lượng</div>
-            <div class="thoigian">thoigian</div>
-            <div class="trangthai">trạng thái</div>
-        </div>
-    </div>`;
+        <div class="thongtin">
+            <div class="thongtinsanpham">
+                <div class="hinh">IMG</div>
+                <div class="sanpham">sản phẩm</div>
+                <div class="dongia">đơn giá</div>
+                <div class="soluong">số lượng</div>
+                <div class="size">Size</div>
+                <div class="thoigian">thoigian</div>
+                <div class="trangthai">trạng thái</div>
+            </div>
+        </div>`;
 
     let thongtin = document.querySelector('.thongtin');
     User.forEach(user => {
@@ -42,10 +43,10 @@ donhang.addEventListener('click', function() {
                 <div class="soluong">${donhang.quantity}</div>
                 <div class="size">${donhang.size}</div>
                 <div class="thoigian">${donhang.time}</div>
+                <div class="trangthai">${donhang.status}</div>
                 <div class="switch-toggle">
                     <label>
-                    <input type="checkbox" onclick="updateOrderStatus(${donhang.ID},'${donhang.size}')">
-
+                        <input type="checkbox" onclick="updateOrderStatus('${user.username}', ${donhang.ID},'${donhang.size}')">
                         <span></span>
                     </label>
                 </div>
@@ -54,18 +55,20 @@ donhang.addEventListener('click', function() {
     });
 });
 
-function updateOrderStatus (ID, size)  {
-    console.log("Updating order status for ID:", ID, "and size:", size);
-    const orderIndex = User.findIndex((user) => user.cart && user.cart.some((donhang) => donhang.ID === ID && donhang.size === size && donhang.status === "chưa xử lí"));
+function updateOrderStatus(username, ID, size) {
+    User.forEach((user, userIndex) => {
+        if (user.username === username && user.cart) {
+            user.cart.forEach((donhang, orderIndex) => {
+                if (donhang.ID === ID && donhang.size === size && donhang.status === "Chưa xử lí") {
+                    // Update order status to "đã xử lí"
+                    User[userIndex].cart[orderIndex].status = "Đã xử lí";
+                }
+            });
+        }
+    });
 
-    if (orderIndex !== -1) {
-        const user = User[orderIndex];
-        const order = user.cart.find((donhang) => donhang.ID === ID && donhang.size === size && donhang.status === "chưa xử lí");
-
-        // Update order status to "đã xử lí"
-        order.status = "đã xử lí";
-
-        localStorage.setItem("user", JSON.stringify(User));
-        alert('Đã xử lí đơn hàng!');
-    }
-};
+    localStorage.setItem("user", JSON.stringify(User));
+    alert('Đã xử lí đơn hàng!');
+    // You may want to refresh the UI here or reload the page to reflect the status change.
+    // Example: location.reload();
+}
